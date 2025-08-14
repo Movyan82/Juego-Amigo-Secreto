e71717bdf4aaec294bcd6b322bb33751bdba77ce
Movyan82 <77217729+Movyan82@users.noreply.github.com>
2025-08-14 09:31:39 -0500

    Funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo () {
+    let nuevoAmigo = document.getElementById(`amigo`).value.trim();
+
+    if (nuevoAmigo === "") {
+        alert(`Celda vacía, por favor ingrese el nombre de un amigo.`);
+        return;
+    } if (amigos.includes(nuevoAmigo)) {
+        alert(`El nombre ingresado, ya se encuentra en la lista de amigos.`);
+        return;
+
+    } else {
+        amigos.push(nuevoAmigo);
+        document.getElementById(`amigo`).value = "";
+        mostrarLista();
+    }
+}
+
+function mostrarLista(){
+    let lista = document.getElementById(`listaAmigos`);
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        list.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert(`No hay amigos en la lista para el sorteo. Ingrese los nombres de los amigos.`);
+        return;
+    } 
+    let aleatorio
+}
