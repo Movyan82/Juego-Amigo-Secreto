ee6b6a2bdf1e9882616034b679dd006fb2a2c029
Movyan82 <77217729+Movyan82@users.noreply.github.com>
2025-08-14 09:37:17 -0500

    Pruebas amigos array

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function agregarAmigo () {
     } else {
         amigos.push(nuevoAmigo);
         document.getElementById(`amigo`).value = "";
+        console.log(amigos);
         mostrarLista();
     }
 }
